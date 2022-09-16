f5d5cd6fea155b9ca0c21d9c9dfdc631567eb997
Denta Hidayat <denta.dent53@gmail.com>
2022-09-16 16:17:33 +0700

    adding data and import header

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello, World!\n")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+iris = pd.read_csv('winequality-red-csv')
+
+
