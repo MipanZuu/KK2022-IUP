f6acc4cadc87e736708a06c8f02bfd9888302bfd
Denta Hidayat <denta.dent53@gmail.com>
2022-09-16 16:56:04 +0700

    importing csv file

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.cluster import KMeans
+from warnings import filterwarnings
 from sklearn import datasets
 
-iris = pd.read_csv('winequality-red-csv')
+dataImport = pd.read_csv('winequality-red.csv')
+print("Successfully Imported Data!")
+dataImport.head()
+
+print(dataImport.shape)
+dataImport.describe(include='all')
+
 
 
